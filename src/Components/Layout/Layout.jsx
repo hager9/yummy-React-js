c52de437f5b75d8fa45182c8f9f66de885752cdc
hager9 <hm4500176@gmail.com>
2025-06-16 17:34:27 +0300

    ..

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout() {
 
   return (
     <>
-      <div className="container py-5 my-5">
+      <div className="container py-5 my-md-5">
         <Outlet />
         <div>{network}</div>
       </div>
